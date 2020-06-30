9e3f863534f5ac1709e83b0e72ca65afc964b6c5
Prachi95 <Prachi95@users.noreply.github.com>
2020-06-30 18:55:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Prachi',                   # Type in your name
   author_email = 'prachirane95@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Prachi95/ZS_AdvancedViz_test1',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Prachi95/ZS_AdvancedViz_test1/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Prachi95/ZS_AdvancedViz_test1/archive/v2.0.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
