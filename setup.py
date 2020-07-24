9542bb879c32898e5290ac785aaf568e96b99514
Prachi95 <Prachi95@users.noreply.github.com>
2020-07-24 19:44:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
+          'webcolors'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
